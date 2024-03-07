dd75b74b317a21c4b77719768aca08061b4260d3
liziyi <gg_liziyi@qq.com>
2024-03-07 10:41:13 +0800

    update

--- src/components/LoadResultPage.vue
+++ src/components/LoadResultPage.vue
@@ -3,25 +3,40 @@
 </template>
 
 <script setup lang="ts">
- import * as invokes from "../invokes";
- import { dialog } from "@tauri-apps/api";
+import {
+  useStore,
+  useConfigStore,
+  useTmpConfigStore,
+  useSavedConfigStore,
+  saveConfigTmp,
+  saveConfig,
+} from "../store";
+import * as invokes from "../invokes";
+import { dialog } from "@tauri-apps/api";
 
- const submitLoad = async () => {
-   let load_path = await dialog.open({
-     defaultPath: "result.psr",
-     filters: [
-       // stand for PostSolver result
-       { name: "Psr Files", extensions: ["psr"] },
-       { name: "All Files", extensions: ["*"] },
-     ],
-   });
-   console.log("loadpath is", load_path);
-   if (load_path === null) {
-     return;
-   }
-   if (Array.isArray(load_path)) {
-     return;
-   }
-   let success = await invokes.loadPostSolveResult(load_path);
- };
+const store = useStore();
+
+const submitLoad = async () => {
+  let load_path = await dialog.open({
+    defaultPath: "result.psr",
+    filters: [
+      // stand for PostSolver result
+      { name: "Psr Files", extensions: ["psr"] },
+      { name: "All Files", extensions: ["*"] },
+    ],
+  });
+  console.log("loadpath is", load_path);
+  if (load_path === null) {
+    return;
+  }
+  if (Array.isArray(load_path)) {
+    return;
+  }
+  let success = await invokes.loadPostSolveResult(load_path);
+  if (success) {
+    store.isSolverFinished = true;
+  } else {
+    console.log("load result failed!");
+  }
+};
 </script>
